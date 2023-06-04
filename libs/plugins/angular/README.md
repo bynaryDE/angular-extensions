f6dd192135712f71cb55501fb0edbc1f947def39
Johannes Homeier <j.homeier@bynary.io>
2023-06-04 14:04:03 +0200

    feat(plugins-angular): add plugins-angular lib

---
+++ libs/plugins/angular/README.md
@@ -0,0 +1,11 @@
+# plugins-angular
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build plugins-angular` to build the library.
+
+## Running unit tests
+
+Run `nx test plugins-angular` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/plugins/angular/jest.config.ts
@@ -0,0 +1,13 @@
+/* eslint-disable */
+export default {
+    displayName: 'plugins-angular',
+    preset: '../../../jest.preset.js',
+    transform: {
+        '^.+\\.[tj]s$': [
+            'ts-jest',
+            { tsconfig: '<rootDir>/tsconfig.spec.json' },
+        ],
+    },
+    moduleFileExtensions: ['ts', 'js', 'html'],
+    coverageDirectory: '../../../coverage/libs/plugins/angular',
+};
