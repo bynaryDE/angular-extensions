e47791e346d4e6855cfa7d690ad50da1ec2b83ec
Johannes Homeier <j.homeier@bynary.io>
2023-11-03 21:02:27 +0100

    feat(demo): improve support for dark color scheme

--- apps/demo/src/app/components/button/button.component.ts
+++ apps/demo/src/app/components/button/button.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, computed, ElementRef, inject, Output, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, Output, ViewEncapsulation } from '@angular/core';
 import { bindAttribute, useAttribute, useBooleanAttribute } from '@bynary/angular-composables/attribute';
 import { provideBaseClass, useModifier, useModifierGroup } from '@bynary/angular-composables/class';
 import { useActivate } from '@bynary/angular-composables/observer';
@@ -18,16 +18,12 @@ import { useActivate } from '@bynary/angular-composables/observer';
 })
 export class ButtonComponent {
 
-    readonly element = inject(ElementRef).nativeElement as HTMLElement;
-
-    readonly role = useAttribute('role', { defaultValue: 'button' });
     readonly type = useAttribute('type', { defaultValue: 'button' });
     readonly disabled = useBooleanAttribute('disabled');
 
     readonly loading = useModifier('loading', { applyInitially: false });
 
     readonly appearance = useModifierGroup('solid');
-    readonly color = useModifierGroup(undefined, { prefix: 'color' });
 
     @Output()
     readonly active = useActivate({ click: true, keydown: [ 'Enter' ] });
