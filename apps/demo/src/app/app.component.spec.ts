5955ab372cd053182f18a1d63682612cb1d87a22
Johannes Homeier <j.homeier@bynary.io>
2023-11-07 10:31:12 +0100

    test(demo): fix broken tests

--- apps/demo/src/app/app.component.spec.ts
+++ apps/demo/src/app/app.component.spec.ts
@@ -18,10 +18,10 @@ describe('AppComponent', () => {
     it('should render title', () => {
         const compiled = fixture.nativeElement as HTMLElement;
 
-        expect(compiled.querySelector('h1')?.textContent).toContain('demo');
+        expect(compiled.querySelector('h1')?.textContent).toContain('@bynary/angular-composables');
     });
 
     it(`should have as title 'demo'`, () => {
-        expect(fixture.componentInstance.title()).toEqual('demo');
+        expect(fixture.componentInstance.title()).toEqual('@bynary/angular-composables');
     });
 });
