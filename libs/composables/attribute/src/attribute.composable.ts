157d2ad36d8e177a1444f139434b394bd805964c
Johannes Homeier <j.homeier@bynary.io>
2023-11-15 09:11:50 +0100

    docs(composables/attribute): add examples to jsdoc

--- libs/composables/attribute/src/attribute.composable.ts
+++ libs/composables/attribute/src/attribute.composable.ts
@@ -8,17 +8,77 @@ export interface IBindAttributeOptions {
      * The namespace of the attribute
      *
      * @example
-     * a namespace `my` will result in an attribute `my:attribute-name`
+     * a namespace `xyz` will result in an attribute `my:<attribute-name>`:
+     *
+     * ```ts
+     * const label = useAttribute('label', { namespace: 'xyz', initialValue: 'baz' });
+     * // or
+     * const label = signal('baz');
+     * bindAttribute('label', mySignal, { namespace: 'xyz' });
+     * ```
+     *
+     * will result in
+     *
+     * ```html
+     * <my-component xyz:label="baz"></my-component>
+     * ```
      */
     namespace?: string;
 
     /**
      * The default value of the attribute, as a fallback, when no initial value has been defined and no value has been assigned in the DOM
+     *
+     * @example Applying a default value
+     * ```ts
+     * const label = useAttribute('label', { defaultValue: 'baz' });
+     * // or
+     * const label = signal<string | undefined>(undefined);
+     * bindAttribute('label', label, { defaultValue: 'baz' });
+     * ```
+     * ```html
+     * <my-component></my-component>
+     * ```
+     *
+     * will result in
+     *
+     * ```html
+     * <my-component label="baz"></my-component>
+     * ```
+     *
+     * @example Will not override if a value has been explicitly assigned in the DOM
+     * ```ts
+     * const label = useAttribute('label', { defaultValue: 'baz' });
+     * // or
+     * const label = signal<string | undefined>(undefined);
+     * bindAttribute('label', mySignal, { defaultValue: 'baz' });
+     * ```
+     * ```html
+     * <my-component label="foo"></my-component>
+     * ```
+     *
+     * will result in
+     *
+     * ```html
+     * <my-component label="foo"></my-component>
+     * ```
      */
     defaultValue?: string;
 
     /**
      * The host element on which the attribute should be bound
+     *
+     * @example
+     * ```ts
+     * import { useAttribute } from '@bynary/composables/attribute';
+     *
+     * @Component({
+     *     selector: 'my-component'
+     * })
+     * class MyComponent {
+     *
+     *     label = useAttribute('label', { host: document.body });
+     * }
+     * ```
      */
     host?: Element;
 }
@@ -29,26 +89,47 @@ export interface IBindAttributeOptions {
 export interface IUseAttributeOptions extends IBindAttributeOptions {
     /**
      * The initial value of the attribute, overriding the value assigned in the DOM
+     *
+     * @example
+     * ```ts
+     * const label = useAttribute('label', { initialValue: 'bar' });
+     * ```
+     *
+     * ```html
+     * <my-component #myComponent label="foo"></my-component>
+     * ```
+     *
+     * will result in
+     *
+     * ```html
+     * <my-component label="bar"></my-component>
+     * ```
+     *
      */
     initialValue?: string | null;
 }
 
+type NormalizedUseAttributeOptions = IUseAttributeOptions & Required<Pick<IUseAttributeOptions, 'host'>>;
 /**
  * Normalizes the given options.
  *
  * @param options - The options to normalize
+ * @internal
  */
-const normalizeUseAttributeOptions = (options?: IUseAttributeOptions) => ({
+const normalizeUseAttributeOptions = (options?: IUseAttributeOptions): NormalizedUseAttributeOptions => ({
     ...(options ?? {}),
     host: options?.host ?? inject(ElementRef).nativeElement as HTMLElement
 });
+
+type NormalizedBindAttributeOptions = IBindAttributeOptions & Required<Pick<IBindAttributeOptions, 'host'>>;
 
 /**
  * Normalizes the given options.
  *
  * @param options - The options to normalize
+ * @internal
  */
-const normalizeBindAttributeOptions = (options?: IBindAttributeOptions) => ({
+const normalizeBindAttributeOptions = (options?: IBindAttributeOptions): NormalizedBindAttributeOptions => ({
     ...(options ?? {}),
     host: options?.host ?? inject(ElementRef).nativeElement as HTMLElement
 });
@@ -56,13 +137,73 @@ const normalizeBindAttributeOptions = (options?: IBindAttributeOptions) => ({
 /**
  * Creates a signal that binds its value as an attribute on the host element.
  *
+ * @example Default usage
+ *
+ * By default, this composable will read the value of the attribute from the usage in the template
+ *
+ * ```ts
+ * import { useAttribute } from '@bynary/composables/attribute';
+ *
+ * @Component({
+ *     selector: 'my-component'
+ * })
+ * class MyComponent {
+ *
+ *     label = useAttribute('label');
+ * }
+ * ```
+ *
+ * ```html
+ * <my-component #myComponent label="foo"></my-component>
+ * ```
+ *
+ * will result in
+ *
+ * ```html
+ * <my-component label="foo"></my-component>
+ * ```
+ *
+ * @example Programmatically setting the value
+ *
+ * You can also change the value of the attribute programmatically by using the returned signal:
+ *
+ * ```ts
+ * import { useAttribute } from '@bynary/composables/attribute';
+ *
+ * @Component({
+ *     selector: 'my-component'
+ * })
+ * class MyComponent {
+ *
+ *     label = useAttribute('label');
+ *
+ *     constructor() {
+ *         this.label.set('programmatically set value');
+ *     }
+ * }
+ * ```
+ *
+ * ```html
+ * <my-component #myComponent></my-component>
+ *
+ * {{ myComponent.label }}
+ * ```
+ *
+ * will result in
+ *
+ * ```html
+ * <my-component label="programmatically set value"></my-component>
+ *
+ * programmatically set value
+ * ```
+ *
  * @param attributeName - The name of the attribute
  * @param options - A set of {@link IUseAttributeOptions options}
  */
-export const useAttribute = (
+export function useAttribute (
     attributeName: string,
     options?: IUseAttributeOptions
-): WritableSignal<string | null | undefined> => {
+): WritableSignal<string | null | undefined> {
     const { namespace, initialValue, defaultValue, host } =
         normalizeUseAttributeOptions(options);
 
@@ -79,7 +220,31 @@ export const useAttribute = (
 };
 
 /**
- * Binds the value of the given signal as an attribute on the host element.
+ * Binds an attribute to the host element. Similar to `useAttribute`, but accepts a signal as an input instead of creating a new one and won't read the value from the template.
+ * Will return the signal that has been passed in.
+ *
+ * @example
+ * ```ts
+ * import { bindAttribute } from '@bynary/composables/attribute';
+ *
+ * @Component({
+ *     selector: 'my-component'
+ * })
+ * class MyComponent {
+ *
+ *     label = signal('foo');
+ *
+ *     constructor() {
+ *         bindAttribute('label', this.label);
+ *     }
+ * }
+ * ```
+ *
+ * will result in
+ *
+ * ```html
+ * <my-component label="label"></my-component>
+ * ```
  *
  * @param attributeName - The name of the attribute
  * @param value - The signal whose value should be bound
--- libs/composables/attribute/src/boolean-attribute.composable.ts
+++ libs/composables/attribute/src/boolean-attribute.composable.ts
@@ -10,17 +10,72 @@ export interface IBindBooleanAttributeOptions {
      * The namespace of the attribute
      *
      * @example
-     * a namespace `my` will result in an attribute `my:attribute-name`
+     * a namespace `xyz` will result in an attribute `xyz:<attribute-name>`:
+     *
+     * ```ts
+     * const isDisabled = useBooleanAttribute('disabled', { namespace: 'xyz', initialValue: true });
+     * // or
+     * const isDisabled = signal(true);
+     * bindBooleanAttribute('disabled', isDisabled, { namespace: 'xyz' });
+     * ```
+     *
+     * will result in
+     *
+     * ```html
+     * <my-component xyz:disabled></my-component>
+     * ```
      */
     namespace?: string;
 
     /**
      * The default value of the attribute, as a fallback, when no initial value has been defined and no value has been assigned in the DOM
+     *
+     * @example Applying a default value
+     * ```ts
+     * const isDisabled = useBooleanAttribute('disabled', { defaultValue: true });
+     * // or
+     * const isDisabled = signal<boolean | undefined>(undefined);
+     * bindBooleanAttribute('disabled', isDisabled, { defaultValue: true });
+     * ```
+     * ```html
+     * <my-component></my-component>
+     * ```
+     *
+     * will result in
+     *
+     * ```html
+     * <my-component disabled></my-component>
+     * ```
+     *
+     * @example Will not override if a value has been explicitly assigned in the DOM
+     * ```ts
+     * const isDisabled = useAttribute('disabled', { defaultValue: false });
+     * // or
+     * const isDisabled = signal<boolean | undefined>(undefined);
+     * bindAttribute('disabled', isDisabled, { defaultValue: false });
+     * ```
+     * ```html
+     * <my-component disabled></my-component>
+     * ```
+     *
+     * will result in
+     *
+     * ```html
+     * <my-component disabled></my-component>
+     * ```
      */
     defaultValue?: boolean;
 
     /**
-     * The host element on which the attribute should be bound
+     * The host element on which the attribute should be bound. Can be any HTMLElement.
+     *
+     * @example
+     * ```ts
+     * const isDisabled = useBooleanAttribute('disabled', { host: document.body });
+     * // or
+     * const isDisabled = signal<boolean | undefined>(undefined);
+     * bindBooleanAttribute('disabled', isDisabled, { host: document.body });
+     * ```
      */
     host?: Element;
 }
@@ -28,10 +83,23 @@ export interface IBindBooleanAttributeOptions {
 /**
  * A set of options for {@link useBooleanAttribute}
  */
-export interface IUseBooleanAttributeOptions
-    extends IBindBooleanAttributeOptions {
+export interface IUseBooleanAttributeOptions extends IBindBooleanAttributeOptions {
     /**
      * The initial value of the attribute, overriding the value assigned in the DOM
+     *
+     * @example
+     * ```ts
+     * const isDisabled = useBooleanAttribute('disabled', { initialValue: false });
+     * ```
+     * ```html
+     * <my-component #myComponent disabled></my-component>
+     * ```
+     *
+     * will result in
+     *
+     * ```html
+     * <my-component></my-component>
+     * ```
      */
     initialValue?: boolean;
 }
@@ -40,25 +108,24 @@ export interface IUseBooleanAttributeOptions
  * Normalizes the given options.
  *
  * @param options - The options to normalize
+ * @internal
  */
-const normalizeUseBooleanAttributeOptions = (
-    options?: IUseBooleanAttributeOptions
-) => options ?? {};
+const normalizeUseBooleanAttributeOptions = (options?: IUseBooleanAttributeOptions): IUseBooleanAttributeOptions => options ?? {};
 
 /**
  * Normalizes the given options.
  *
  * @param options - The options to normalize
+ * @internal
  */
-const normalizeBindBooleanAttributeOptions = (
-    options?: IBindBooleanAttributeOptions
-) => options ?? {};
+const normalizeBindBooleanAttributeOptions = (options?: IBindBooleanAttributeOptions): IBindBooleanAttributeOptions => options ?? {};
 
 /**
  * Converts the given value to an attribute value.
  * Boolean Attributes are set, when the value is an empty string and removed, when the value is `null` or `undefined`.
  *
  * @param value - an empty string, when the attribute should be set,`null` when the attribute should be explicitly removed, `undefined` when the attribute may be overridden
+ * @internal
  */
 const toAttributeValue = (value: boolean | undefined) => {
     if (value === true) {
@@ -72,6 +139,19 @@ const toAttributeValue = (value: boolean | undefined) => {
 
 /**
  * Creates a signal that binds its value as a boolean attribute on the host element.
+ *
+ * @example
+ * ```ts
+ * import { useBooleanAttribute } from '@bynary/composables/attribute';
+ *
+ * @Component({
+ *     selector: 'my-component'
+ * })
+ * class MyComponent {
+ *
+ *     isDisabled = useBooleanAttribute('disabled');
+ * }
+ * ```
  *
  * @param attributeName - The name of the attribute
  * @param options - A set of {@link IUseBooleanAttributeOptions options}
@@ -93,6 +173,23 @@ export const useBooleanAttribute = (
 
 /**
  * Binds the value of the given signal as a boolean attribute on the host element.
+ *
+ * @example
+ * ```ts
+ * import { bindBooleanAttribute } from '@bynary/composables/attribute';
+ *
+ * @Component({
+ *     selector: 'my-component'
+ * })
+ * class MyComponent {
+ *
+ *     isDisabled = signal(false);
+ *
+ *     constructor() {
+ *         bindBooleanAttribute('disabled', this.isDisabled);
+ *     }
+ * }
+ * ```
  *
  * @param attributeName - The name of the attribute
  * @param value - The signal to bind
