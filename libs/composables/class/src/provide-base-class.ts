59358e2650ebc0ad6dcc929b5dbf1e14913a48ed
Johannes Homeier <j.homeier@bynary.io>
2023-11-18 11:29:57 +0100

    docs(composables/class): add example

--- libs/composables/class/src/provide-base-class.ts
+++ libs/composables/class/src/provide-base-class.ts
@@ -7,9 +7,23 @@ export const BASE_CLASS = new InjectionToken<string>('@bynary/composables/class.
 
 /**
  * Provide a base CSS class that should be applied to a host.
+ * The base class can be injected using the {@link BASE_CLASS} token.
+ * This token will be used by {@link useModifier} and {@link useModifierGroup} to generate modifier classes.
  *
  * @see useModifier
  * @see useModifierGroup
+ *
+ * @example
+ * ```ts
+ * @Component({
+ *    selector: 'my-component',
+ *    providers: [
+ *        provideBaseClass('my-component')
+ *    ]
+ * })
+ * class MyComponent {
+ *     baseClass = inject(BASE_CLASS); // 'my-component'
+ * }
  *
  * @param baseClass - The base class to provide.
  */
