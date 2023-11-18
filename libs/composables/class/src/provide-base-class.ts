87944d55813e91e983edb824ed6fc0d1145f817f
Johannes Homeier <j.homeier@bynary.io>
2023-11-18 11:29:36 +0100

    refactor(composables/class): change token description

--- libs/composables/class/src/provide-base-class.ts
+++ libs/composables/class/src/provide-base-class.ts
@@ -3,7 +3,7 @@ import { InjectionToken } from '@angular/core';
 /**
  * Injection token for providing a base CSS class.
  */
-export const BASE_CLASS = new InjectionToken<string>('base-class');
+export const BASE_CLASS = new InjectionToken<string>('@bynary/composables/class.base-class');
 
 /**
  * Provide a base CSS class that should be applied to a host.
