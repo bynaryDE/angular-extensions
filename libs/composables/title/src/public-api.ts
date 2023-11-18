25e5900a65dacd4ed49aca942db96c98fdb9f0d4
Johannes Homeier <j.homeier@bynary.io>
2023-11-18 10:01:57 +0100

    feat(composables/title): add bindTitle and documentation

--- libs/composables/title/src/public-api.ts
+++ libs/composables/title/src/public-api.ts
@@ -1,5 +1,7 @@
 /**
  * @module @bynary/composables/title
+ *
+ * Composables for managing the title of the page.
  */
 
 export * from './title.composable';
--- libs/composables/title/src/title.composable.ts
+++ libs/composables/title/src/title.composable.ts
@@ -1,9 +1,28 @@
-import { effect, inject, signal } from '@angular/core';
+import { effect, inject, Signal, signal } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
-export const useTitle = (initialValue?: string) => {
+/**
+ * Binds the value of the signal to the title of the page.
+ *
+ * @example
+ * ```ts
+ * @Component({
+ *    selector: 'my-component',
+ *    template: ''
+ * })
+ * class MyComponent {
+ *     name = signal('Jane');
+ *
+ *     constructor() {
+ *         bindTitle(computed(() => `Hello ${this.name()}`));
+ *     }
+ * }
+ * ```
+ *
+ * @param value - The signal to bind to the title.
+ */
+export const bindTitle = <T extends Signal<string>>(value: T): T => {
     const title = inject(Title);
-    const value = signal(initialValue ?? title.getTitle());
 
     effect(() => {
         title.setTitle(value());
@@ -13,18 +32,41 @@ export const useTitle = (initialValue?: string) => {
 };
 
 /**
- * @internal
- * @param name
- * @param initialValue
+ * A signal to change the title of the page.
+ *
+ * @example
+ * ```ts
+ * @Component({
+ *    selector: 'my-component',
+ *    template: ''
+ * })
+ * class MyComponent {
+ *     title = useTitle('Hello World'); // The title of the page is now 'Hello World'
+ *
+ *     greetUser(name: string) {
+ *         this.title.set(`Hello ${name}`); // For a name 'Jane', the title of the page is now 'Hello Jane'
+ *     }
+ * }
+ * ```
+ *
+ * @example
+ * Reading the current title of the page
+ *
+ * ```ts
+ * @Component({
+ *    selector: 'my-component',
+ *    template: ''
+ * })
+ * class MyComponent {
+ *     title = useTitle(); // The initial value of the signal is the current title of the page
+ * }
+ * ```
+ *
+ * @param initialValue - The initial title of the page. Defaults to the current title of the page.
  */
-export const useTitle2 = (name: string, initialValue?: string) => {
+export const useTitle = (initialValue?: string) => {
     const title = inject(Title);
-
     const value = signal(initialValue ?? title.getTitle());
 
-    effect(() => {
-        title.setTitle(value());
-    });
-
-    return value;
+    return bindTitle(value);
 };
