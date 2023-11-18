b21cb94dd9be8ceef572b519c0af6ff3a4606e1d
Johannes Homeier <j.homeier@bynary.io>
2023-11-18 08:40:28 +0100

    refactor(composables/observer): remove obsolete takeUntilDestroyed
    toSignal already handles unsubscribing when the current context gets destroyed

--- libs/composables/observer/src/breakpoint.composable.ts
+++ libs/composables/observer/src/breakpoint.composable.ts
@@ -1,16 +1,13 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { inject } from '@angular/core';
-import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs';
 
 export const useBreakpoint = (breakpoint: string) => {
     const breakpointObserver = inject(BreakpointObserver);
 
     return toSignal(
-        breakpointObserver.observe(breakpoint).pipe(
-            takeUntilDestroyed(),
-            map((v) => v.matches)
-        ),
+        breakpointObserver.observe(breakpoint).pipe(map((v) => v.matches)),
         {
             initialValue: breakpointObserver.isMatched(breakpoint)
         }
